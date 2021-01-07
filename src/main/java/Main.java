c6e1d766994ce8473f8040c5a1a1e9ed367e188b
ZaborskiyAIG <zaborskiyaig@gmail.ru>
2021-01-07 21:15:13 +0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        while(true) {
+            System.out.println("Hello docker");
+            Thread.sleep(1500);
+        }
+    }
+}
